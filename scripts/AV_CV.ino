1f5c4a531f73bc503051bfd9463f71b8eba89c13
Екатерина Лавлинская <lavlinskaya94@internet.ru>
2026-03-29 12:22:28 +0300

    Create AV_CV.ino

---
+++ scripts/AV_CV.ino
@@ -0,0 +1,18 @@
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else if (command == '0') {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+}
